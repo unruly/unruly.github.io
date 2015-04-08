c817fa9b7691a8599e1774b30568bdd5726e2135
Jahed Ahmed <jahed.public@gmail.com>
2015-04-08 17:36:59 +0100

    Made blog page look more like a blog page.

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,24 +1,51 @@
 var pages = pages || {};
 
 pages.index = pages.index || (function() {
-	function addDevelopers(template) {
+	function addContributors(template) {
 		$.getJSON('./javascripts/data/devs.json').success(function(devsConfig) {
 			$.getJSON('http://jahed.unruly.co/github/unruly/members').success(function(members) {
-				var $devsContainer = $('#devsContainer');
+				var $contributors = $('#contributors');
 
-				members.forEach(function(member) {
-					var personHtml = Mustache.render(template, {
-						name: member.login,
-						image: member.avatar_url,
-						githubUrl: member.html_url,
-						links: devsConfig[member.login] ? devsConfig[member.login].links : []
-					});
+				members
+                    .filter(function(member) {
+                        return !!devsConfig[member.login];
+                    })
+                    .forEach(function(member) {
+                        var config = devsConfig[member.login];
+                        var contributorHtml = Mustache.render(template, {
+                            name: config.name || member.login,
+                            image: member.avatar_url,
+                            githubUrl: member.html_url,
+                            links: config ? config.links : []
+                        });
 
-					$devsContainer.append(personHtml);
-				});
+                        $contributors.append(contributorHtml);
+                    });
 			});
 		});
 	}
+
+    function addDevelopers(template) {
+        $.getJSON('./javascripts/data/devs.json').success(function(devsConfig) {
+            $.getJSON('http://jahed.unruly.co/github/unruly/members').success(function(members) {
+                var $developers = $('#developers');
+
+                members
+                    .filter(function(member) {
+                        return !devsConfig[member.login];
+                    })
+                    .forEach(function(member) {
+                        var developerHtml = Mustache.render(template, {
+                            name: member.login,
+                            image: member.avatar_url,
+                            githubUrl: member.html_url
+                        });
+
+                        $developers.append(developerHtml);
+                    });
+            });
+        });
+    }
 
 	function addFeaturedPost(template) {
 		$.getJSON('./javascripts/data/posts.json').success(function(postsConfig) {
@@ -39,53 +66,59 @@ pages.index = pages.index || (function() {
 
 
 	function addLatestPosts(template) {
-		$.getJSON('./javascripts/data/posts.json').success(function(postsConfig) {
-			$.ajax({
-				url: 'http://video.cdn-unrulymedia-5.com/unruly.github.io/atom.xml',
-				cache: false
-			}).success(function(feedDocument) {
-				var $feed = $(feedDocument).find('feed'),
-					$entries = $feed.find('feed > entry:lt(6)'),
-					$featuredPostContainer = $('#postsContainer');
+        $.getJSON('./javascripts/data/posts.json').success(function (postsConfig) {
+            $.ajax({
+                url: 'http://video.cdn-unrulymedia-5.com/unruly.github.io/atom.xml',
+                cache: false
+            }).success(function (feedDocument) {
+                var $feed = $(feedDocument).find('feed'),
+                    $entries = $feed.find('feed > entry:lt(6)'),
+                    $featuredPostContainer = $('#postsContainer');
 
-				function summariseText(text, length) {
-					var summary = text.substr(0, length);
-					if (/^\S/.test(text.substr(length))) {
-						summary = summary.replace(/\s+\S*$/, "");
-					}
-					return summary
-						.replace(/[_\W]$/, "")
-						.trim();
-				}
+                function summariseText(text, length) {
+                    var summary = text.substr(0, length);
+                    if (/^\S/.test(text.substr(length))) {
+                        summary = summary.replace(/\s+\S*$/, "");
+                    }
+                    return summary
+                        .replace(/[_\W]$/, "")
+                        .trim() + '&hellip;';
+                }
 
-				$entries.each(function() {
-					var $entry = $(this),
-						rawContent = $entry.find('> content').text(),
-						$content = $('<div>').append($.parseHTML(rawContent)),
-						summary = summariseText($content.text(), 120),
-						image = $content.find('img:first-of-type').attr('src');
+                $entries.each(function () {
+                    var $entry = $(this),
+                        rawContent = $entry.find('> content').text(),
+                        $content = $('<div>').append($.parseHTML(rawContent)),
+                        image = $content.find('img:first-of-type').attr('src'),
+                        summary = $('<div>')
+                            .append($(
+                                summariseText($('<div>').append(
+                                    $content.find('p, blockquote, ul, ol, h1,h2,h3,h4,h5')
+                                ).html(), 1000)
+                            ))
+                            .html();
 
-					var postHtml = Mustache.render(template, {
-						title: $entry.find('> title').text(),
-						date: moment($entry.find('> updated').text()).fromNow(),
-						summary: summary,
-						author: {
-							name: $entry.find('> author > name').text()
-						},
-						url: $entry.find('> link').attr('href'),
-						image: image
-					});
+                    var postHtml = Mustache.render(template, {
+                        title: $entry.find('> title').text(),
+                        summary: summary,
+                        date: moment($entry.find('> updated').text()).fromNow(),
+                        author: {
+                            name: $entry.find('> author > name').text()
+                        },
+                        url: $entry.find('> link').attr('href'),
+                        image: image
+                    });
 
-					$featuredPostContainer.append(postHtml);
-				});
-			});
-		});
-	}
-
+                    $featuredPostContainer.append(postHtml);
+                });
+            });
+        });
+    }
 	function init() {
 		$.get('./javascripts/partials/featured-post.mustache').success(addFeaturedPost);
 		$.get('./javascripts/partials/post.mustache').success(addLatestPosts);
-		$.get('./javascripts/partials/person.mustache').success(addDevelopers);
+        $.get('./javascripts/partials/contributor.mustache').success(addContributors);
+        $.get('./javascripts/partials/developer.mustache').success(addDevelopers);
 	}
 
 	return {
