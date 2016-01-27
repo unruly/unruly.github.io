671c68fe168df999416652b5a8c3ae3d0ede1050
Hannes & Jahed <dev@unrulymedia.com>
2016-01-27 11:23:20 +0000

    Added forks to repo page

--- source/javascripts/repositories.js
+++ source/javascripts/repositories.js
@@ -36,9 +36,39 @@ pages.repositories = pages.repositories || (function() {
 		});
 	}
 
+    function addForks(template) {
+        $.getJSON('http://api.tech.unruly.co/github/unruly/forks').success(function(forks) {
+            var $forksContainer = $('#forksContainer');
+
+            forks.map(function(repo) {
+                repo.pushed_at = new Date(repo.pushed_at);
+                return repo;
+            }).sort(function sortByUpdateDate(repoA, repoB) {
+                return  repoB.pushed_at - repoA.pushed_at;
+            }).forEach(function(repo) {
+                repo.language = repo.language || 'Mixed';
+                repo.homepage = repo.homepage || repo.html_url;
+
+                var repoHtml = Mustache.render(template, {
+                    name: repo.name,
+                    description: repo.description,
+                    url: repo.html_url,
+                    language: repo.language,
+                    language_class: repo.language.toLowerCase(),
+                    fork: repo.fork
+                });
+
+                $forksContainer.append(repoHtml);
+            });
+
+            $forksContainer.find('.loading').remove();
+        });
+    }
 
 	function init() {
-        $.get('./javascripts/partials/repo.mustache').success(addRepositories);
+        $.get('./javascripts/partials/repo.mustache')
+            .success(addRepositories)
+            .success(addForks);
 	}
 
 	return {
